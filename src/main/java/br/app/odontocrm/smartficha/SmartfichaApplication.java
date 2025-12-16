5c3d346eb15bc03321a85d4ddc175519723a2672
Thales A Contratres <thalesaugusto312@gmail.com>
2025-12-16 05:37:52 -0300

    First Commit - Spring Boot Starter files

---
+++ src/main/java/br/app/odontocrm/smartficha/SmartfichaApplication.java
@@ -0,0 +1,13 @@
+package br.app.odontocrm.smartficha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartfichaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartfichaApplication.class, args);
+	}
+
+}
