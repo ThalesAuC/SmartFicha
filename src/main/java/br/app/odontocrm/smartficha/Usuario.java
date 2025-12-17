704e16031b650bdeea13018c0949eeaa2592ca0e
Thales A Contratres <thalesaugusto312@gmail.com>
2025-12-17 01:34:33 -0300

    Feat login page, early stage

---
+++ src/main/java/br/app/odontocrm/smartficha/Usuario.java
@@ -0,0 +1,25 @@
+package br.app.odontocrm.smartficha;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotEmpty;
+
+@Entity
+public class Usuario {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotEmpty
+	private String nome;
+	
+	@NotEmpty
+	private String email;
+	
+	@NotEmpty
+	private String senha;
+	
+	
+}
